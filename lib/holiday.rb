7384bf11257dd594778562bf12e9bc8733ecd062
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-04 18:33:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,5 +77,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  list = []
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      if supplies.include?("BBQ")
+        list << holiday
+      end
+    end
+  end
+  list
 end
