ab90b5170866556cba19461fc49faebe4ef40d2e
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-04 18:14:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday, supplies|
       message = []
-      holiday.to_s.split("_").each{|e| e.empty? e.capitalize << message}
+      holiday.to_s.partition("_").each{|e| e.empty? e.capitalize << message}
         puts "  #{message.join(" ")}: #{supplies.join(", ")}"
     end
   end
