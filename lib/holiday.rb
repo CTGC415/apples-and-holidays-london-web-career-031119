24228b3d73c1d9bbabb2a20869bcb803a960e0aa
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-04 17:25:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts season
+    puts season.to_s.capitalize
     holiday.each do |holiday, supplies|
       puts "#{holiday} #{supplies}"
     end
